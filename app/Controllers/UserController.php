ac76de46a861154711556b0f620b7cdc554244df
drice82 <xujiabin82@gmail.com>
2017-11-06 22:22:38 +0800

    ssr link

--- app/Controllers/UserController.php
+++ app/Controllers/UserController.php
@@ -71,8 +71,6 @@ class UserController extends BaseController
         $ary['obfsparam'] = "www.microsoft.com";
         $json = json_encode($ary);
         $json_show = json_encode($ary, JSON_PRETTY_PRINT);
-        // $ssurl = $ary['method'] . ":" . $ary['password'] . "@" . $ary['server'] . ":" . $ary['server_port'];
-        // $ssqr = "ss://" . base64_encode($ssurl);
 
         $ssrurl_prefix = $ary['server'] . ":443:auth_aes128_md5:aes-128-ctr:tls1.2_ticket_auth:" . base64_encode("pubpassword");
         $ssrurl_suffix = '/?' . "obfsparam=" . base64_encode($ary['server_port'] . ":" . $ary['password']) . "&protoparam=" . base64_encode($ary['server_port'] . ":" . $ary['password']) . "&remarks=" . base64_encode($ary['server']) . "&group=" . base64_encode("GoodByeFW");  

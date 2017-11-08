c9922619ba7267bb4371d01059ebbdf42bb393de
drice82 <xujiabin82@gmail.com>
2017-11-08 21:21:17 +0800

    checkExpireTime

--- app/Command/XCat.php
+++ app/Command/XCat.php
@@ -33,6 +33,8 @@ class XCat
                 return $this->resetTraffic();
             case("sendDiaryMail"):
                 return DailyMail::sendDailyMail();
+            case("checkExpireTime"):
+                return $this->checkExpireTime();
             default:
                 return $this->defaultAction();
         }

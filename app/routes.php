c8e48a9330a374ce56cdd61f278a1232e98e0468
drice82 <xujiabin82@gmail.com>
2017-11-10 14:57:06 +0800

    node2

--- app/routes.php
+++ app/routes.php
@@ -44,6 +44,7 @@ $app->group('/user', function () {
     $this->post('/checkin', 'App\Controllers\UserController:doCheckin');
     $this->get('/node', 'App\Controllers\UserController:node');
     $this->get('/node/{id}', 'App\Controllers\UserController:nodeInfo');
+    $this->get('/node2', 'App\Controllers\UserController:node2');
     $this->get('/profile', 'App\Controllers\UserController:profile');
     $this->get('/invite', 'App\Controllers\UserController:invite');
     $this->post('/invite', 'App\Controllers\UserController:doInvite');
